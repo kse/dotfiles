e383965947343b3ae80df13c99b5ff2edd9bea35
Kasper Eenberg <kse@mahavira.dk>
2012-10-29 17:29:08 +0100

    Updated nasa imotd script to daemonize and more.

--- scripts/aw_nasa_bg.pl
+++ scripts/aw_nasa_bg.pl
@@ -6,72 +6,151 @@ use XML::Simple qw(:strict);
 use Data::Dumper;
 use File::Copy;
 use Text::Wrap qw(wrap);
+use Proc::Daemon;
+use Encode;
 
 # Simple script to change the awesome background to the nasa
 # image of the day.
 #
 # Call this from crontab, and it should just work. But only for one
 # user on a machine at a time (it uses global filename).
-#
-# I added 'cat ~/.message' to my bashrc. This means i get the description
-# and title of the image whenever i start a shell.
-# This works for me.
 
 my $feed_path = "http://www.nasa.gov/rss/lg_image_of_the_day.rss";
 
-my $tmp_image = '/tmp/bckground.tmp';
-my $image     = '/tmp/bckground.jpg';
+my $tmp_image = '/tmp/awesome_iotd_bckground.tmp';
+my $image     = '/tmp/awesome_iotd_bckground.jpg';
 
-my $data = `wget -O - $feed_path 2> /dev/null`;
+my $lockfile  = '/tmp/awesome_iotd_bckground.pid';
+my $logfile   = '/tmp/awesome_iotd_bckground.log';
 
-if(($? >> 8) != 0) {
-	if(-t STDOUT) {
-		print "Error fetching page with wget\n";
-	}
+# Default an hour.
+my $sleeptime = 3600;
+
+open(my $log, '>>', $logfile) or die "Unable to open logfile: $!";
+
+$SIG{__DIE__} = sub {
+	print $log "Die: $_[0]\n";
+	die $_[0];
 	exit 1;
+};
+
+$SIG{__WARN__} = sub {
+	#print $log "Received signal $_[1]\n";
+	print $log "Warn: " . $_[0] . "\n";
+};
+
+$SIG{'INT'} = sub {
+	print $log "Interruped by signal\n";
+	exit 1;
+};
+
+if(-e $lockfile) {
+	open(my $LFH, '<', $lockfile);
+	my $pid = <$LFH>;
+	chomp($pid);
+	my $ret;
+
+	$ret = kill(0, ($pid));
+
+	# Process is running.
+	if($ret) {
+		my $process = `ps -p $pid -o command`;
+		if($process =~ m/\Q$0\E/) {
+			print "Program is already running, exiting\n";
+			exit 1;
+		} else {
+			# Someone else has taken our old PID
+		}
+	} else {
+		#Process is not running, success
+	}
 }
 
-my $ref = XMLin($data, ForceArray => [], KeyAttr => {});
+Proc::Daemon->init();
 
-if(!$ref) {
-	if(-t STDOUT) {
-		print "Error parsing XML\n";
+# Write the lockfile.
+open(my $LFH, '>', $lockfile) or die $!;
+print $log "Writing pid $$ to lockfile\n";
+print $LFH $$;
+close($LFH);
+
+while(1) {
+	my $data = `wget -O - $feed_path 2> /dev/null`;
+
+	if(($? >> 8) != 0) {
+		if(-t STDOUT) {
+			print $log "Error fetching page with wget\n";
+		}
+		#exit 1;
+		$sleeptime = 500;
+		next;
 	}
-	exit 1;
-}
 
-my $link = $ref->{channel}->{item}->{enclosure}->{url};
-my $title = $ref->{channel}->{item}->{title};
-my $description = $ref->{channel}->{item}->{description};
+	my $ref = XMLin($data, ForceArray => [], KeyAttr => {});
 
-if(!$link || !$title || !$description) {
-	if(-t STDOUT) {
-		print "No data in hashref\n"
+	if(!$ref) {
+		if(-t STDOUT) {
+			print $log "Error parsing XML\n";
+		}
+		#exit 1;
+		$sleeptime = 500;
+		next;
 	}
-	exit 1;
-}
 
-`wget -O $tmp_image $link 2> /dev/null`;
+	my $link = $ref->{channel}->{item}->{enclosure}->{url};
+	my $title = $ref->{channel}->{item}->{title};
+	my $description = $ref->{channel}->{item}->{description};
 
-if(($? >> 8) != 0) {
-	if(-t STDOUT) {
-		print "Error fetching image\n";
+	if(!$link || !$title || !$description) {
+		if(-t STDOUT) {
+			print $log "No data in hashref\n"
+		}
+		$sleeptime = 500;
+		next;
+		#exit 1;
 	}
-	if(-e $tmp_image) {
-		unlink($tmp_image);
+
+	`wget -O $tmp_image $link 2> /dev/null`;
+
+	if(($? >> 8) != 0) {
+		if(-t STDOUT) {
+			print $log "Error fetching image\n";
+		}
+		if(-e $tmp_image) {
+			unlink($tmp_image);
+		}
+		$sleeptime = 500;
+		next;
+		#exit 1;
 	}
-	exit 1;
-}
 
-move($tmp_image, $image);
+	move($tmp_image, $image);
 
-`awsetbg -a $image`;
+	#print $log "Before awset\n";
+	#my $message = `awsetbg -a $image`;
+	my $message = `feh --bg-max $image`;
+	#print $log "Just asfter awset\n";
+	if(($? >> 8) != 0) {
+		print $log "Error settings background: $message\n";
+		#print $log "Inside error awset\n";
+		next;
+	} else {
+		print $log "Updated background image\n";
+	}
 
-if($ENV{HOME}) {
-	open(my $msgfh, '>', $ENV{HOME} . '/.message') or die $!;
+	#print $log "After awset\n";
+	if($ENV{HOME}) {
+		open(my $msgfh, '>', $ENV{HOME} . '/.message') or die $!;
 
-	print $msgfh wrap('', '', $title)       . "\n";
-	print $msgfh wrap('', '', $description) . "\n";
+		$description = decode_utf8($description);
 
-	close($msgfh);
+		print $msgfh wrap('', '', "-- $title --")       . "\n";
+		print $msgfh wrap('', '', $description) . "\n";
+
+		close($msgfh);
+	}
+
+	$sleeptime = 3600;
+} continue {
+	sleep($sleeptime);
 }
